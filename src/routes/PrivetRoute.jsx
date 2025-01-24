88fc32b0172dea4f3503bcd7929cc4dadcae8926
dilara369 <dilarahossenara@gmail.com>
2025-01-24 23:42:53 +0600

    PrivateRoute

--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -4,11 +4,10 @@ import { Navigate, useLocation } from "react-router-dom";
 import Loading from './../components/Loading';
 
 
-// eslint-disable-next-line react/prop-types
 const PrivetRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-//   console.log(location);
+
 
   if (loading) {
     return <Loading/>;
